8fcaace1c710b28c9214e31b0b03ee098bc6e704
Manuel <manuelvidal097@gmail.com>
2023-11-12 04:52:05 -0300

    Main para testear el desarrollo

---
+++ adoo/test/App.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.util.*;
+
+import color.ConsoleColors;
+import java.time.Duration;
+import controladores.*;
+import modelos.dtos.*;
+import singleton.Escaner;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        ControllerEjemplar controladorEjemplar = ControllerEjemplar.getInstancia();
+                    
+
+            
+    }
+}
