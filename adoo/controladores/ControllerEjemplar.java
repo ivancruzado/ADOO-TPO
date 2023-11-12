0e95344807856fa32641f41d5758edfdf48ba950
Manuel <manuelvidal097@gmail.com>
2023-11-12 04:52:45 -0300

    Creacion de las instancias

--- adoo/controladores/ControllerEjemplar.java
+++ adoo/controladores/ControllerEjemplar.java
@@ -6,24 +6,21 @@ import modelos.Ejemplar;
 
 public class ControllerEjemplar {
 
+    private ArrayList <Ejemplar> Productos;
+    private static ControllerEjemplar instancia;
     /**
      * Default constructor
      */
-    public ControllerEjemplar() {
+    public static ControllerEjemplar getInstancia() {
+        if (instancia == null)
+            instancia = new ControllerEjemplar();
+        return instancia;
     }
 
-    /**
-     * 
-     */
-    private ArrayList <Ejemplar> Productos;
-
-    /**
-     * @param titulo 
-     * @param autor 
-     * @param fechaPublicacion 
-     * @param tipo? 
-     * @return
-     */
+    private ControllerEjemplar() {
+        Productos = new ArrayList<>();
+    }
+  
     public int crearEjemplar(String titulo, String autor, String fechaPublicacion, String tipo) {
         // TODO implement here
         return 0;
