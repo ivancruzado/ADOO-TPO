430fdc43cec2039b634e346fd920c887445c5a15
Manuel <manuelvidal097@gmail.com>
2023-11-20 17:44:30 -0300

    arreglo

--- adoo/adapter/notificador/sms/AdapterSMS.java
+++ adoo/adapter/notificador/sms/AdapterSMS.java
@@ -13,7 +13,7 @@ public class AdapterSMS implements IAdapterSMS{
     public void enviarNotificacion(NotificadorDTO notificacion) {
         System.out.println(ConsoleColors.GREEN + 
                 "Se ha enviado un mensaje por SMS al numero " + ConsoleColors.GREEN_BOLD + notificacion.getDestinatario().getTelefono() + ConsoleColors.GREEN 
-                        + " del Cliente Adoptante " + ConsoleColors.GREEN_BOLD + notificacion.getDestinatario().getNombre()
+                        + " del Socio " + ConsoleColors.GREEN_BOLD + notificacion.getDestinatario().getNombre()
                         + " " + notificacion.getDestinatario().getApellido() + ConsoleColors.GREEN +
                         " en la fecha " + ConsoleColors.GREEN_BOLD + notificacion.getFecha() + ConsoleColors.GREEN + " con el mensaje: " + ConsoleColors.GREEN_BOLD + "\"" + notificacion.getMensaje() + "\"" + ConsoleColors.RESET
         );
--- adoo/adapter/notificador/whatsapp/AdapterWhatsApp.java
+++ adoo/adapter/notificador/whatsapp/AdapterWhatsApp.java
@@ -15,7 +15,7 @@ public class AdapterWhatsApp implements IAdapterWhatsApp{
     public void enviarNotificacion(NotificadorDTO notificacion) {
         System.out.println(ConsoleColors.GREEN + 
                 "Se ha enviado un mensaje por Whatsapp al numero " + ConsoleColors.GREEN_BOLD + notificacion.getDestinatario().getTelefono() + ConsoleColors.GREEN
-                        + " del Cliente Adoptante " + ConsoleColors.GREEN_BOLD + notificacion.getDestinatario().getNombre()
+                        + " del Socio " + ConsoleColors.GREEN_BOLD + notificacion.getDestinatario().getNombre()
                         + " " + notificacion.getDestinatario().getApellido() + ConsoleColors.GREEN +
                         " en la fecha " + ConsoleColors.GREEN_BOLD + notificacion.getFecha() + ConsoleColors.GREEN + " con el mensaje: "+ ConsoleColors.GREEN_BOLD + "\"" + notificacion.getMensaje() + "\"" + ConsoleColors.RESET
         );
